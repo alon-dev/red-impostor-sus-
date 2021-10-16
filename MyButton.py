796331fad86957330958955e24275b1230ca1a35
Alon <alonen15@gmail.com>
2021-10-16 17:39:06 +0300

    mognus

--- MyButton.py
+++ MyButton.py
@@ -1,12 +1,15 @@
 import tkinter as tk
 from tkinter import PhotoImage
+from PIL import Image, ImageTk
 
 
 class MyButton(tk.Button):
     def __init__(self, master, color):
         self.DEFAULT_IMAGE = PhotoImage()
+        self.WHITE_IMAGE = PhotoImage(file="img/1.png")
         super().__init__(master, bg=color, image=self.DEFAULT_IMAGE, width=70, height=70, command= self.on_button_click)
-        self.WHITE_IMAGE = PhotoImage(r"img\white_queen.png", width=70, height=70)
 
     def on_button_click(self):
+        print("clicked!")
         self.config(image=self.WHITE_IMAGE)
+
