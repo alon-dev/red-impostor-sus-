303f5bdc925ca34c3d450fb99678141aaca5aca5
Alon <alonen15@gmail.com>
2021-10-24 12:30:36 +0300

    gay tkinter

--- MyButton.py
+++ MyButton.py
@@ -5,8 +5,14 @@ from PIL import Image, ImageTk
 
 
 class MyButton(tk.Button):
-    def __init__(self, master, color, i, j):
+    def __init__(self, master, color, i, j, command):
+        self.command = command
+        self.i = i
+        self.j = j
         self.DEFAULT_IMAGE = PhotoImage()
         self.WHITE_IMAGE = PhotoImage(file="img/1.png")
-        super().__init__(master, bg=color, image=self.DEFAULT_IMAGE, width=70, height=70, highlightthickness = 0, bd = 0)
-
+        super().__init__(master, bg=color, image=self.DEFAULT_IMAGE, width=70, height=70, highlightthickness = 0, bd = 0, command= self.my_command)
+    def my_command(self):
+        self.command(self)
+    def reset(self):
+        self.config(image=self.DEFAULT_IMAGE)
--- view.py
+++ view.py
@@ -1,10 +1,12 @@
-from tkinter import StringVar
-from tkinter import ttk
+from tkinter import PhotoImage, StringVar
+from tkinter import ttk, messagebox
+from Controller import Controller
 from MyButton import MyButton
+from time import sleep
 
 class View:
     def __init__(self, master):
-        self.controller = None
+        self.controller = Controller()
         self.master = master
         self.main_frame = ttk.Frame(self.master)
         self.boardFrm = ttk.Frame(self.main_frame)
@@ -17,22 +19,19 @@ class View:
             if i%2 == 0:
                 for j in range(8):
                     if j % 2 == 0:
-                        self.buttonsArray[i][j] = MyButton(self.boardFrm, 'white', i, j)
+                        self.buttonsArray[i][j] = MyButton(self.boardFrm, 'white', i, j, self.on_button_click)
                         self.buttonsArray[i][j].grid(row=i,column=j)
                     else:
-                        self.buttonsArray[i][j] = MyButton(self.boardFrm, 'black', i, j)
+                        self.buttonsArray[i][j] = MyButton(self.boardFrm, 'black', i, j, self.on_button_click)
                         self.buttonsArray[i][j].grid(row=i,column=j)
-                    self.buttonsArray[i][j].configure(command= lambda: self.on_button_click())
             else:
                 for j in range(8):
                     if j % 2 == 1:
-                        self.buttonsArray[i][j] = MyButton(self.boardFrm, 'white', i, j)
+                        self.buttonsArray[i][j] = MyButton(self.boardFrm, 'white', i, j, self.on_button_click)
                         self.buttonsArray[i][j].grid(row=i,column=j)
                     else:
-                        self.buttonsArray[i][j] = MyButton(self.boardFrm, 'black', i, j)
+                        self.buttonsArray[i][j] = MyButton(self.boardFrm, 'black', i, j, self.on_button_click)
                         self.buttonsArray[i][j].grid(row=i,column=j)
-                    b= self.buttonsArray[i][j]
-                    self.buttonsArray[i][j].configure(command= lambda: self.on_button_click(b))
 
         
         self.inputFrm = ttk.Frame(self.main_frame)
@@ -43,20 +42,38 @@ class View:
         self.dropdownLabel = ttk.Label(self.inputFrm, text="Number of Queens:")
         self.strvar = StringVar(self.inputFrm)
         self.strvar.set("One")
-        self.dropdown_menu = ttk.OptionMenu(self.inputFrm, self.strvar, "One", "Two", "Three")
+        self.dropdown_menu = ttk.OptionMenu(self.inputFrm, self.strvar, *["One", "One", "Two", "Three"])
         
         self.msLabel.pack()
         self.number_of_ms_input.pack()
         self.dropdownLabel.pack()
         self.dropdown_menu.pack()
 
+        #dumb fuck nigger homo
         self.buttonFrm = ttk.Frame(self.main_frame)
         self.buttonFrm.grid(columnspan=2, row=1, column=0)
-        self.runButton = ttk.Button(self.buttonFrm, text="Run!")
+        self.runButton = ttk.Button(self.buttonFrm, text="Run!", command=self.run)
         self.runButton.pack(side="left")
         self.resetButton = ttk.Button(self.buttonFrm, text="Reset")
         self.resetButton.pack(side="right")
         self.main_frame.pack()
     def on_button_click(self, b):
-        print("clicked!")
         b.config(image=b.WHITE_IMAGE)
+        self.controller.determinant_place(b.i, b.j)
+    def run(self):
+        try:
+            number_of_ms = int(self.number_of_ms_input.get())
+        except Exception as e:
+            messagebox.showerror("Error!", "Please enter a valid ms timeout")
+            return
+        results = self.controller.show_results()
+        if not results:
+            messagebox.showerror("Error!", "No solutions found.")
+            return
+        self.show_results(results, number_of_ms)
+    def show_results(self, results, number_of_ms):
+        for res in results:
+            for pair in res:
+                print(pair) 
+                self.on_button_click(self.buttonsArray[pair[0]][pair[1]])
+                sleep(number_of_ms)
