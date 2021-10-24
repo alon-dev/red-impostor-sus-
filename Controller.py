1539a19a350c8f2e95f00406ee7e2fe57fb5329a
Alon <alonen15@gmail.com>
2021-10-24 12:30:28 +0300

    gay tkinter

--- Controller.py
+++ Controller.py
@@ -1,14 +1,23 @@
-from .Model import Model
-from .view import View
+from Model import Model
 
 
 class Controller:
 
     def __init__(self):
         self.model = Model()
-        self.view = View()
 
     
     def determinant_place(self, i, j):
         self.model.arr[j] = i
         self.model.user_selcted[j] = True
+    
+    def show_results(self):
+        ret = []
+        self.model.rec_backt(0)
+        for sol in self.model.results:
+            solarr = []
+            for i in range(8):
+                solarr.append((i,sol[i]))
+            ret.append(solarr)
+        return ret
+    
